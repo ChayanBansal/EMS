4e46b9b4092cfb2145dde2d3eae4bd3226235069
Chayan Bansal <bansalc10@gmail.com>
2017-12-29 22:20:33 +0530

    Signed-off-by: Chayan Bansal <bansalc10@gmail.com>

--- includes/update_marks.php
+++ includes/update_marks.php
@@ -6,6 +6,7 @@
     {
         try{
             mysqli_autocommit($conn,FALSE);
+            mysqli_begin_transaction($conn);
             $create_check_id="INSERT INTO checking(operator_id, remark) VALUES(".$_SESSION['operator_id'].",'".$_POST['remark']."')"; //check_id	operator_id	timestamp	remark
             $create_check_id_run=mysqli_query($conn,$create_check_id);
             $check_id=mysqli_insert_id($conn);
@@ -15,13 +16,12 @@
         $update_score_run = mysqli_query($conn, $update_score);
 
         while ($new_score = mysqli_fetch_assoc($update_score_run)) {
-            $update_record = "UPDATE score SET marks=" . $_POST[$new_score['enrol_no']] . " AND check_id=" . $check_id . " WHERE roll_id=" . $new_score['roll_id'];
+            $update_record = "UPDATE score SET marks=" . $_POST[$new_score['enrol_no']] . " AND check_id=" . $check_id . " WHERE roll_id=" . $new_score['roll_id'] ."AND transaction_id=". $_SESSION['check_transaction_id'];
             $update_record_run = mysqli_query($conn, $update_record);
         }
 
-        $update_auditing = "UPDATE auditing WHERE transaction_id=" . $_SESSION['check_transaction_id'];
+        $update_auditing = "UPDATE auditing SET check_id=".$check_id." WHERE transaction_id=" . $_SESSION['check_transaction_id'];
         $update_auditing_run = mysqli_query($conn, $update_auditing);
-
         mysqli_commit($conn);
         header('location: useroptions.php');
     } catch (Exception $e) {
