04e5543e17b542f1458b720e065aae098f41b3f2
Chayan Bansal <bansalc10@gmail.com>
2017-12-30 15:09:49 +0530

    Signed-off-by: Chayan Bansal <bansalc10@gmail.com>

--- includes/checking.php
+++ includes/checking.php
@@ -12,7 +12,7 @@ if (isset($_POST["check_button"])) {
         $_SESSION['component_id'] = $check_detail['component_id'];
         $_SESSION['checked_by_operator_id'] = $check_detail['operator_id'];
         $_SESSION['remark'] = $check_detail['remark'];
-        if($check_detail['check_id']!=null)
+        if($check_detail['check_id']!='Null')
         {
             $_SESSION['already_checked']=true;
             header('location: useroptions.php');
