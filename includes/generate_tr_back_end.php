fd1d59e00d80c1f3c11c7e541ac037f15234efa0
Chayan Bansal <bansalc10@gmail.com>
2017-12-31 20:27:40 +0530

    Signed-off-by: Chayan Bansal <bansalc10@gmail.com>

---
+++ includes/generate_tr_back_end.php
@@ -0,0 +1,212 @@
+<?php
+    
+    $semester=$_POST['tab_submit'];//Button's value which is clicked 
+    $from_year=$_SESSION['from_year']; //Selected batch relevant to the tr
+    $course_id=$_SESSION['course_id']; //Selected course's id
+
+    $get_roll_id="SELECT roll_id FROM roll_list WHERE semester=".$semester." AND enrol_no IN 
+                (SELECT enrol_no FROM students WHERE course_id=".$course_id." current_sem=".$semester." AND from_year=".$from_year.")"; //CHECK the name of the column current_sem
+    $get_roll_id_run=mysqli_query($conn,$get_roll_id);
+    while($roll_id=mysqli_fetch_assoc($get_roll_id_run)) //$roll_id['roll_id']
+    {
+        $get_sub_id="SELECT sub_id, practical_flag, credits_alloted FROM sub_distribution WHERE sub_code IN
+                    (SELECT sub_code FROM subjects WHERE course_id=".$course_id." AND semester=".$semester.")";
+        $get_sub_id_run=mysqli_query($conn,$get_sub_id); 
+        while($sub_id=mysqli_fetch_assoc($get_sub_id_run)) //$sub_id['sub_id'] $sub_id['practical_flag'] $sub_id['credits_alloted']
+        {
+            if(($sub_id['practical_flag']==0) OR ($sub_id['practical_flag']==1))
+            {
+                if($sub_id['practical_flag']==0) //for theory sub_id
+                {
+                    $get_comp_marks="SELECT component_id, marks FROM score WHERE roll_id=".$roll_id['roll_id']." AND sub_id=".$sub_id['sub_id'];
+                    $get_comp_marks_run=mysqli_query($conn,$get_comp_marks);
+                    while($comp_marks=mysqli_fetch_assoc($get_comp_marks_run)) //$comp_marks['component_id'] $comp_marks['marks']
+                    {
+                        if($comp_marks['component_id']==1)
+                        {
+                            $cat_cap_ia=$comp_marks['marks'];
+                        }
+                        else if($comp_marks['component_id']==2)
+                        {
+                            $endsem=$comp_marks['marks'];
+                        }
+                        else
+                        {
+                            echo('If else condition satisfied here, then there might be an error in the database (because a theory subject_id can only have two above mentioned components)');
+                        }
+                    }
+                }
+                else if($sub_id['practical_flag']==1) //for practical sub_id
+                {   
+                    $cat_cap_ia=0;
+                    $get_comp_marks="SELECT component_id, marks FROM score WHERE roll_id=".$roll_id['roll_id']." AND sub_id=".$sub_id['sub_id'];
+                    $get_comp_marks_run=mysqli_query($conn,$get_comp_marks);
+                    while($comp_marks=mysqli_fetch_assoc($get_comp_marks_run)) //$comp_marks['component_id'] $comp_marks['marks']
+                    {
+                        if($comp_marks['component_id']==3)
+                        {
+                            $cat_cap_ia = $cat_cap_ia + $comp_marks['marks'];
+                        }
+                        else if($comp_marks['component_id']==4)
+                        {
+                            $endsem=$comp_marks['marks'];
+                        }
+                        else if($comp_marks['component_id']==5)
+                        {
+                            $cat_cap_ia = $cat_cap_ia + $comp_marks['marks'];
+                        }
+                        else
+                        {
+                            echo('If else condition satisfied here, then there might be an error in the database (because a practical subject_id can only have two above mentioned components)');
+                        }
+                    }
+                }
+
+                $total = $cat_cap_ia + $endsem;
+                $percentage = (($total*100)/100);
+
+                if ($percentage >= 91 and $percentage <= 100) 
+                {
+					$grade = 'O';
+					$gp = 10;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 81 and $percentage < 91) 
+                {
+					$grade = 'A+';
+					$gp = 9;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 71 and $percentage < 81) 
+                {
+					$grade = 'A';
+					$gp = 8;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 61 and $percentage < 71) 
+                {
+					$grade = 'B+';
+					$gp = 7;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 51 and $percentage < 61) 
+                {
+					$grade = 'B';
+					$gp = 6;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 41 and $percentage < 51) 
+                {
+					$grade = 'C';
+					$gp = 5;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage == 40) 
+                {
+					$grade = 'P';
+					$gp = 4;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage < 40) 
+                {
+					$grade = 'F';
+					$gp = 0;
+					$cr = 0;
+					$gpv = $gp * $cr;
+				}
+
+                $insert_tr="INSERT INTO tr (roll_id, sub_id, cat_cap_ia, endsem, total, percent, grade, gp, cr, gpv) 
+                    VALUES(".$roll_id['roll_id'].", ".$sub_id['sub_id'].", ".$cat_cap_ia.", ".$endsem.", ".$total.", ".$percentage.", '".$grade."', ".$gp.", ".$cr.", ".$gpv.")";
+                $insert_tr_run=mysqli_query($conn,$insert_tr);
+                //An entry in a practical or theory subject done till here 
+            }
+            else if($sub_id['practical_flag']==2) //for IE sub_id
+            {
+                $get_comp_marks="SELECT component_id, marks FROM score WHERE roll_id=".$roll_id['roll_id']." AND sub_id=".$sub_id['sub_id'];
+                $get_comp_marks_run=mysqli_query($conn,$get_comp_marks);
+                while($comp_marks=mysqli_fetch_assoc($get_comp_marks_run)) //$comp_marks['component_id'] $comp_marks['marks']
+                {
+                    if($comp_marks['component_id']==6)
+                    {
+                        $ie = $comp_marks['marks'];
+                    }
+                    else
+                    {
+                        echo('If else condition satisfied here, then there might be an error in the database (because an IE subject_id can only have one component)');
+                    }
+                }    
+                    $total = $ie;
+                    $percentage = (($total*100)/100);
+                
+                if ($percentage >= 91 and $percentage <= 100) 
+                {
+					$grade = 'O';
+					$gp = 10;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 81 and $percentage < 91) 
+                {
+					$grade = 'A+';
+					$gp = 9;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 71 and $percentage < 81) 
+                {
+					$grade = 'A';
+					$gp = 8;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 61 and $percentage < 71) 
+                {
+					$grade = 'B+';
+					$gp = 7;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 51 and $percentage < 61) 
+                {
+					$grade = 'B';
+					$gp = 6;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage >= 41 and $percentage < 51) 
+                {
+					$grade = 'C';
+					$gp = 5;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage == 40) 
+                {
+					$grade = 'P';
+					$gp = 4;
+					$cr = $sub_id['credits_alloted'];
+					$gpv = $gp * $cr;
+                } 
+                else if ($percentage < 40) 
+                {
+					$grade = 'F';
+					$gp = 0;
+					$cr = 0;
+					$gpv = $gp * $cr;
+                }
+                
+                $insert_tr="INSERT INTO tr (roll_id, sub_id, ie, total, percent, grade, gp, cr, gpv) 
+                    VALUES(".$roll_id['roll_id'].", ".$sub_id['sub_id'].", ".$ie.", ".$total.", ".$percentage.", '".$grade."', ".$gp.", ".$cr.", ".$gpv.")";
+                $insert_tr_run=mysqli_query($conn,$insert_tr);
+                //An entry in a Internal Examination(IE) subject done here 
+            }
+        }
+    }
+?>
