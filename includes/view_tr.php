dad09d53ecab3a47a0ba4d18504e3fe2ae8972a1
Chayan Bansal <bansalc10@gmail.com>
2018-01-05 01:05:29 +0530

    Signed-off-by: Chayan Bansal <bansalc10@gmail.com>

--- includes/view_tr.php
+++ includes/view_tr.php
@@ -1,17 +1,16 @@
 <?php 
 session_start();
-$course_id=$_SESSION['course_id'];
+/*$course_id=$_SESSION['course_id'];
 $semester=$_SESSION['semester'];
-$from_year=$_SESSION['from_year'];
+$from_year=$_SESSION['from_year'];*/
 
-
-$servername = "";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$host="sql12.freemysqlhosting.net";
+$user="sql12213993";
+$pass="iqpTsClsCq";
+$db="sql12213993";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $db);
 
 // Check connection
 if ($conn->connect_error) {
@@ -19,6 +18,7 @@ if ($conn->connect_error) {
 }
 
 // prepare and bind
+/*
 $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
 $stmt->bind_param("sss", $firstname, $lastname, $email);
 
@@ -27,7 +27,17 @@ $firstname = "John";
 $lastname = "Doe";
 $email = "john@example.com";
 $stmt->execute();
+*/
+//getting subjects
+$get_student_list = $conn->prepare("SELECT roll_id FROM roll_list WHERE semester=? AND enrol_no=?");
+$get_student_list->bind_param('is',$semester,$enrol_no);
 
-
+$semester=3;
+$enrol_no='2016AB001022';
+$get_student_list->execute();
+if($get_student_list->num_rows()>0)
+{
+    echo('success');
+}
 
 ?>
